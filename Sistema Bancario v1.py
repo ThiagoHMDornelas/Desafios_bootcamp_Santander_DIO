08316d6b623c203a5fead401f8276c12b8250fcc
ThiagoHMDornelas <thiagohmdornelas@gmail.com>
2025-07-17 14:10:19 -0300

    criação de classes para posterior atualização

--- Sistema Bancario v1.py
+++ Sistema Bancario v1.py
@@ -1,3 +1,165 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_Conta(self, conta):
+        self.contas.append(conta)
+
+
+class PessoaFisica(Cliente):
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        super().__init__(endereco)
+
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = '0001'
+        self._cliente = cliente
+        self._historico = Historico()
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @property
+    def agencia(self):
+        return self._agencia
+
+    @property
+    def cliente(self):
+        return self._cliente
+
+    @property
+    def historico(self):
+        return self._historico
+    
+    def saldo():
+        pass
+
+    def nova_conta(cliente, numero):
+        pass
+
+    def sacar(self, rlValor):
+        rlSaldo = self._saldo
+        boSaldoExcedido = rlValor > rlSaldo
+
+        if boSaldoExcedido:
+            print("\nERRO! Saldo insuficiente para sacar o vlr. informado!")
+        elif rlValor > 0:
+            self._saldo -= rlValor
+#            stExtrato += f"Saque:\t\tR$ {rlValor:.2f}\n"
+#            itQtdSaque += 1
+            print("\nSaque realizado com sucesso!")
+            return True
+        else:
+            print("\nERRO! O valor de saque informado deve ser maior do que 0(zero).")
+
+        return False
+
+    def depositar(self, rlVlrDeposito):    
+        if rlVlrDeposito > 0:
+            self._saldo += rlVlrDeposito
+#            stExtrato += f"Depósito:\tR$ {rlVlrDeposito:.2f}\n"
+            print("\nDepósito realizado com sucesso!")
+            return True
+        else:
+            print("\nERRO! O valor de depósito deve ser maior do que 0(zero).")
+            return False
+
+        
+class ContaCorrente(Conta):
+    def __init__(self, numero, cliente, VlrLimite=500, QtdLimiteSaques=3):
+        self.limite = VlrLimite
+        self.limite_saques = QtdLimiteSaques
+        super().__init__ (numero, cliente)   
+
+    def info_conta(self):
+        print(f"""\
+            Agência: {self._agencia}
+            C/C    : {self._numero}
+            Titular: {self._cliente}
+        """)
+        print("*************************")        
+
+
+# Classe igual a do Professor Felipe (DIO). 
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
+
+
+# Classe igual a do Professor Felipe (DIO). 
+class Transacao(ABC):
+    @property
+    @abstractmethod    
+    def valor(self):
+        pass
+    
+    @classmethod
+    def registrar(self, conta):
+        pass
+
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+
 menu = """
 
 [d]  Depositar
@@ -9,6 +171,7 @@ menu = """
 [q]  Sair
 
 => """
+
 def depositar(rlSaldo, rlVlrDeposito, stExtrato, /):    
     if rlVlrDeposito > 0:
         rlSaldo += rlVlrDeposito
